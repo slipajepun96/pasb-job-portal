cda93e627e5470baee84aa67c593a4d6b722089b
slipajepun96 <slipa_jepun96@outlook.com>
2025-01-07 22:05:50 +0800

    test webhook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
                   <path d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002" stroke-width="2"></path>
               </svg>
           </span>
-        bersama kami. test</p>
+        bersama kami. hahaha</p>
         
         
 
