5729acf179d1038c073ae6dd3e501f9c1eec87d9
slipajepun96 <slipa_jepun96@outlook.com>
2025-01-21 22:18:27 +0800

    minor edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 @endif
 
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-8">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Kerjaya</h1>
+        <h1 class="mb-4 text-xl md:text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-4xl">Kerjaya di PKPP Agro</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">Mari 
           <span class="text-lime-500 mx-1 font-bold text-lg relative inline-block stroke-current">
               membangun
